8d4809d94426a24de51e4f5a7790559a617d3c1c
Toshiba <Toshiba@Toshiba-PC.esprit.tn>
2014-03-30 17:03:55 +0100

    First COmmit

---
+++ Sprint2/src/sprint2/GoldenCage.java
@@ -0,0 +1,18 @@
+package sprint2;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author Toshiba
+ */
+public class GoldenCage extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}

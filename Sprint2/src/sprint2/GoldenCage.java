b7d12758bf8cb72336098072005d9c3c79c0efb1
Toshiba <Toshiba@Toshiba-PC.esprit.tn>
2014-03-30 18:09:05 +0100

    Added comments

--- Sprint2/src/sprint2/GoldenCage.java
+++ Sprint2/src/sprint2/GoldenCage.java
@@ -1,18 +1,129 @@
 package sprint2;
 
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
 import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
 
-/**
- * @author Toshiba
- */
-public class GoldenCage extends MIDlet {
 
+
+public class GoldenCage extends MIDlet implements CommandListener, ItemStateListener , Runnable {
+
+    Display disp = Display.getDisplay(this);
+
+    /**  Declarations Cyrine  **/
+    /** FIN Declarations Cyrine  **/
+    
+    /**  Declarations Omar  **/
+    /**  FIN Declarations Omar  **/
+
+    /**  Declarations Khaled  **/
+    /** FIN  Declarations Khaled  **/
+   
+    /**  Declarations Karim  **/
+    /** FIN  Declarations Karim  **/
+    
+    /**  Declarations Youssef  **/
+    /** FIN  Declarations Youssef  **/
+    
+    /**  Declarations Raed  **/
+    /** FIN  Declarations Raed  **/
+    
     public void startApp() {
+        
+    /**  StartUP Cyrine  **/
+    /** FIN StartUP Cyrine  **/
+    
+    /**  StartUP Omar  **/
+    /**  FIN StartUP Omar  **/
+
+    /**  StartUP Khaled  **/
+    /** FIN  StartUP Khaled  **/
+   
+    /**  StartUP Karim  **/
+    /** FIN  StartUP Karim  **/
+    
+    /**  StartUP Youssef  **/
+    /** FIN  StartUP Youssef  **/
+    
+    /**  StartUP Raed  **/
+    /** FIN  StartUP Raed  **/
+    
     }
     
     public void pauseApp() {
     }
     
     public void destroyApp(boolean unconditional) {
+    }
+
+    public void commandAction(Command c, Displayable d) {
+
+    /**  CmdAction Cyrine  **/
+    /** FIN CmdAction Cyrine  **/
+    
+    /**  CmdAction Omar  **/
+    /**  FIN CmdAction Omar  **/
+
+    /**  CmdAction Khaled  **/
+    /** FIN  CmdAction Khaled  **/
+   
+    /**  CmdAction Karim  **/
+    /** FIN  CmdAction Karim  **/
+    
+    /**  CmdAction Youssef  **/
+    /** FIN  CmdAction Youssef  **/
+    
+    /**  CmdAction Raed  **/
+    /** FIN  CmdAction Raed  **/
+    
+    }
+
+    public void run() {
+
+    /**  Run Cyrine  **/
+    /** FIN Run Cyrine  **/
+    
+    /**  Run Omar  **/
+    /**  FIN Run Omar  **/
+
+    /**  Run Khaled  **/
+    /** FIN  Run Khaled  **/
+   
+    /**  Run Karim  **/
+    /** FIN  Run Karim  **/
+    
+    /**  Run Youssef  **/
+    /** FIN  Run Youssef  **/
+    
+    /**  Run Raed  **/
+    /** FIN  Run Raed  **/
+    
+    }
+
+    public void itemStateChanged(Item item) {
+
+    
+    /**  ItmState Cyrine  **/
+    /** FIN ItmState Cyrine  **/
+    
+    /**  ItmState Omar  **/
+    /**  FIN ItmState Omar  **/
+
+    /**  ItmState Khaled  **/
+    /** FIN  ItmState Khaled  **/
+   
+    /**  ItmState Karim  **/
+    /** FIN  ItmState Karim  **/
+    
+    /**  ItmState Youssef  **/
+    /** FIN  ItmState Youssef  **/
+    
+    /**  ItmState Raed  **/
+    /** FIN  ItmState Raed  **/
+    
+
+    
     }
 }
